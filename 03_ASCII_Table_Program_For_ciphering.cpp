8298eb8e82fe22b57a0fe3cef5cd830d56a3a544
Park <piaowei041@163.com>
2022-01-17 11:38:24 +0800

    Add ciphering

--- 03_ASCII_Table_Program_For_ciphering.cpp
+++ 03_ASCII_Table_Program_For_ciphering.cpp
@@ -3,10 +3,17 @@ using namespace std;
 
 int main()
 {
-    cout << (int)'a' << endl;
-    cout << int('a') << endl;
-    cout << int('A') << endl;
-    cout << char(65) << endl;
+    char c1, c2, c3, c4, c5;
+    cout << "Enter 5 leters: ";
+    cin >> c1 >> c2 >> c3 >> c4 >> c5;
+
+    int i1, i2, i3, i4, i5;
+
+    i1 = int(c1), i2 = int(c2), i3 = int(c3), i4 = int(c4), i5 = int(c5);
+
+    cout << "ASCII message: " << int(c1) << " " << int(c2) << " " << int(c3) << " " << int(c4) << " " << int(c5) << endl;
+
+    cout << "deciphering is " << char(i1) << "  " << char(i2) << "    " << char(i3) << "    " << char(i4) << "    " << char(i5);
 
     system("pause>0");
 }
