e675fca7f025c1d9bd5189a909fab280d465d16f
Park <piaowei041@163.com>
2022-01-17 10:39:32 +0800

    overflow

--- 02_Variables_DataType_Overflow_Size.cpp
+++ 02_Variables_DataType_Overflow_Size.cpp
@@ -4,24 +4,10 @@ using namespace std;
 int main()
 {
 
-    int yearOfBirth = 1995;
-    char gender = 'f';
-    bool isOlderThan18 = true;
-    float averageGrad = 4.5;
-    double balance = 45678945856;
-
-    cout << "Size of int is " << sizeof(int) << " bytes\n";
-    //-1,-2,...,-2147483648
-    cout << "Int min value is " << INT_MIN << endl;
-    //0,+1,+2,...,+2147483647
-    cout << "Int max value is " << INT_MAX << endl;
-
-    cout << "Size of unsigned int is " << sizeof(unsigned int) << "bytes\n";
-    cout << "UInt max value is " << UINT_MAX << endl;
-    cout << "Size of bool is " << sizeof(bool) << " bytes\n";
-    cout << "Size of char is " << sizeof(char) << " bytes\n";
-    cout << "size of float is " << sizeof(float) << " bytes\n";
-    cout << "Size of double is " << sizeof(double) << " bytes\n";
+    int intMax = INT_MAX;
+    cout << intMax << endl;
+    cout << intMax + 1 << endl;
+    cout << "min Int is " << INT_MIN << endl;
 
     system("pause>0");
 
