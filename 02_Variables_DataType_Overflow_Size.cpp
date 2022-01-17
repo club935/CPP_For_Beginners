9b9255ece688fcd714a5fcf4d128322c10ecb58d
Park <piaowei041@163.com>
2022-01-17 10:32:55 +0800

    size of variables

--- 02_Variables_DataType_Overflow_Size.cpp
+++ 02_Variables_DataType_Overflow_Size.cpp
@@ -4,14 +4,24 @@ using namespace std;
 int main()
 {
 
-    float annualSalary;
-    cout << "Please enter your annual salary ";
-    cin >> annualSalary;
-    float monthlySalary = annualSalary / 12;
-    cout << "Your monthly salary is " << monthlySalary << endl;
-    cout << "In 10 years you will earn " << annualSalary * 10;
+    int yearOfBirth = 1995;
+    char gender = 'f';
+    bool isOlderThan18 = true;
+    float averageGrad = 4.5;
+    double balance = 45678945856;
 
-    char character = 'a';
+    cout << "Size of int is " << sizeof(int) << " bytes\n";
+    //-1,-2,...,-2147483648
+    cout << "Int min value is " << INT_MIN << endl;
+    //0,+1,+2,...,+2147483647
+    cout << "Int max value is " << INT_MAX << endl;
+
+    cout << "Size of unsigned int is " << sizeof(unsigned int) << "bytes\n";
+    cout << "UInt max value is " << UINT_MAX << endl;
+    cout << "Size of bool is " << sizeof(bool) << " bytes\n";
+    cout << "Size of char is " << sizeof(char) << " bytes\n";
+    cout << "size of float is " << sizeof(float) << " bytes\n";
+    cout << "Size of double is " << sizeof(double) << " bytes\n";
 
     system("pause>0");
 
