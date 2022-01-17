22cef60be7025569c1a4530c6f1472cf786e87fb
Park <piaowei041@163.com>
2022-01-17 10:11:11 +0800

    Add 02 Variables...

--- 01_Firsrt_Program_Hello_World.cpp
+++ 01_Firsrt_Program_Hello_World.cpp
@@ -1,3 +1,4 @@
+//building a code is converting source code that you've written into a code that your machine can understand
 #include <iostream>
 using namespace std; //reference this namespace on a globle level
 
@@ -7,6 +8,7 @@ int main()
 
     cout << "My name is Saldina."<<endl;
     cout << "Today is Friday.";
+    cout << "Have a nice day.";
 
     system("pause>0");
     
