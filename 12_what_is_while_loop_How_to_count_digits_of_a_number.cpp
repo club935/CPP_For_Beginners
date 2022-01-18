a48fd7249cd4a8e6c46d2e4f870ea4631b66f67a
Park <piaowei041@163.com>
2022-01-18 20:07:32 +0800

    what is while loop, How to count digits of a num

---
+++ 12_what_is_while_loop_How_to_count_digits_of_a_number.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{   
+    //count digits of a number
+    int number;
+    cout << "Number: ";
+    cin >> number;
+
+    if(number==0)
+        cout << "You have entered 0.\n";
+    else{
+        
+        if(number<0)
+            number = -1 * number;
+            
+        int counter = 0;
+        while(number>0){
+            //number = number / 10;
+            number /= 10;
+            counter++;
+        }
+        cout << "Number contains " << counter << " digits\n";
+    }
+
+    system("pause>0");
+}
